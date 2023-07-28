dfa159ad87e22401487f0fe63670899af3a0f301
José Lucas <josluc2016@gmail.com>
2023-07-28 00:12:06 -0300

    feat: media contact

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,12 @@
-module.exports.home = (application, req, res) => {
-    res.render('home', { api_url: process.env.API_URL });
+const { getFromApi } = require("../helpers/utils");
+
+module.exports.home = async (application, req, res) => {
+
+    const camas = await getFromApi('category=CAMAS&limit=4')
+    const brinquedos = await getFromApi('category=brinquedo&limit=4')
+    const acessorios = await getFromApi('category=acessorios&limit=4')
+
+    res.render('home', { acessorios, camas, brinquedos });
 }
 
 module.exports.acessories = (application, req, res) => {
---
+++ src/helpers/utils.js
@@ -0,0 +1,47 @@
+async function getFromApi(category = '') {
+    var myHeaders = new Headers();
+
+    var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    const url = `${process.env.API_URL}?${category}`
+
+    const data = await fetch(url, myInit)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            return json.data
+        });
+
+    return data
+}
+
+async function getFromApiById(id) {
+    var myHeaders = new Headers();
+
+    var myInit = {
+        method: 'GET',
+        headers: myHeaders,
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    const url = `${process.env.API_URL}/${id}`
+
+    const data = await fetch(url, myInit)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (json) {
+            return json
+        });
+
+    return data
+}
+
+module.exports = { getFromApi, getFromApiById }
